7d45fd7523b500d58d2ca4c0fe61499c54e838ba
IHA Yasushi <yasuc18@gmail.com>
2024-05-17 15:13:42 +0900

    keymapsとoptionsの修正及びindent-blanklineのエラーへの対処。
    indent-blanklineのエラーはcommit値を指定することで回避している。

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -22,59 +22,8 @@ else
   vim.opt.shell = "fish"
 end
 
-vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
+-- vim.opt.backupskip = { "/tmp/*", "/private/tmp/*" }
 vim.opt.inccommand = "split"
-vim.opt.smarttab = true
 vim.opt.breakindent = true
--- vim.opt.backspace = { "start", "eol", "indent" }
-vim.opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 vim.opt.path:append { "**" } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { "*/node_modules/*" }
-vim.opt.splitkeep = "cursor"
-vim.opt.mouse = ""
-
--- Undercurl
-vim.cmd [[let &t_Cs = "\e[4:3m"]]
-vim.cmd [[let &t_Ce = "\e[4:0m"]]
-
--- Add asterisks in block comments
-vim.opt.formatoptions:append { "r" }
-
-vim.cmd "let g:netrw_liststyle = 3"
-
--- line numbers
-vim.opt.relativenumber = true -- show relative line numbers
-vim.opt.number = true -- shows absolute line number on cursor line (when relative number is on)
-
--- tabs & indentation
-vim.opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-vim.opt.shiftwidth = 2 -- 2 spaces for indent width
-vim.opt.expandtab = true -- expand tab to spaces
-vim.opt.autoindent = true -- copy indent from current line when starting new one
-
--- line wrapping
-vim.opt.wrap = false -- disable line wrapping
-
--- search settings
-vim.opt.ignorecase = true -- ignore case when searching
-vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-
--- cursor line
-vim.opt.cursorline = true -- highlight the current cursor line
-
--- appearance
-
--- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes" -- show sign column so that text doesn't shift
-
--- clipboard
-vim.opt.clipboard:append "unnamedplus" -- use system clipboard as default register
-
--- split windows
-vim.opt.splitright = true -- split vertical window to the right
-vim.opt.splitbelow = true -- split horizontal window to the bottom
-
--- turn off swapfile
-vim.opt.swapfile = false
---
+++ lua/plugins/ibl.lua
@@ -0,0 +1,7 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  -- commit = "29be0919b91fb59eca9e90690d76014233392bef",
+  commit = "3d08501caef2329aba5121b753e903904088f7e6",
+  opts = {},
+}
