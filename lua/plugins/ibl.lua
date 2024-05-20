168fcb7f904092e35952ac60d190b9e330466fb7
IHA Yasushi <yasuc18@gmail.com>
2024-05-20 11:19:11 +0900

    indent-blanklineのcommitをコメント & img-clipのキーマップ変更。

--- lua/plugins/ibl.lua
+++ lua/plugins/ibl.lua
@@ -2,6 +2,6 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   -- commit = "29be0919b91fb59eca9e90690d76014233392bef",
-  commit = "3d08501caef2329aba5121b753e903904088f7e6",
+  -- commit = "3d08501caef2329aba5121b753e903904088f7e6",
   opts = {},
 }
