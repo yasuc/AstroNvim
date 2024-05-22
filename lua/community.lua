ef9e9542017b744fc1b7178bc57533bdd5124d12
IHA Yasushi <yasuc18@gmail.com>
2024-05-22 15:57:29 +0900

    community.luaにcompiler-nvimを追加。

--- lua/community.lua
+++ lua/community.lua
@@ -13,6 +13,7 @@ return {
   { import = "astrocommunity.indent.indent-blankline-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.code-runner.compiler-nvim" },
   -- { import = "astrocommunity.editing-support.chatgpt-nvim" },
   -- { import = "astrocommunity.utility.noice-nvim" },
   -- { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
