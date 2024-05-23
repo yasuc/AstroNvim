d197a0d8e6a81ee344d20b81aac793cf35e6e5aa
IHA Yasushi <yasuc18@gmail.com>
2024-05-23 16:07:57 +0900

    communityへ言語関連を追加。

--- lua/plugins/nvim-dap-python.lua
+++
@@ -1,7 +0,0 @@
-if false then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-return {
-  {
-    "mfussenegger/nvim-dap-python",
-    config = function() require("dap-python").setup() end,
-  },
-}
