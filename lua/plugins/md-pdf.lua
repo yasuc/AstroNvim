7126d203111ef787fc1fe2a9a4878ec7d869dfc6
IHA Yasushi <yasuc18@gmail.com>
2024-05-23 13:55:08 +0900

    md-pdfの追加。

---
+++ lua/plugins/md-pdf.lua
@@ -0,0 +1,17 @@
+return {
+  "arminveres/md-pdf.nvim",
+  branch = "main", -- you can assume that main is somewhat stable until releases will be made
+  lazy = true,
+  keys = {
+    {
+      "<leader>,",
+      function() require("md-pdf").convert_md_to_pdf() end,
+      desc = "Markdown preview",
+    },
+  },
+  opts = {
+    pandoc_user_args = {
+      "-t html5",
+    },
+  },
+}
