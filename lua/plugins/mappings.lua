c802c8f2cb03be5322fc6ab6d638e3c679271da9
IHA Yasushi <yasuc18@gmail.com>
2024-05-24 11:36:19 +0900

    buffer間の移動キーをmappings.luaで設定。

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,21 @@
+return {
+  "AstroNvim/astrocore",
+  ---@type AstroCoreOpts
+  opts = {
+    mappings = {
+      -- first key is the mode
+      n = {
+        -- second key is the lefthand side of the map
+        -- mappings seen under group name "Buffer"
+        ["L"] = {
+          function() require("astrocore.buffer").nav(vim.v.count1) end,
+          desc = "Next buffer",
+        },
+        ["H"] = {
+          function() require("astrocore.buffer").nav(-vim.v.count1) end,
+          desc = "Previous buffer",
+        },
+      },
+    },
+  },
+}
