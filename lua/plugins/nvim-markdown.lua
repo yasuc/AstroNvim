e167d062c0c5980362bffe7f572ff93f39fd712c
IHA Yasushi <yasuc18@gmail.com>
2024-05-24 10:09:22 +0900

    markdown関連のプラグイン追加。

---
+++ lua/plugins/nvim-markdown.lua
@@ -0,0 +1,5 @@
+return {
+  "ixru/nvim-markdown",
+  name = "nvim-markdown", -- Only needed if you have another plugin named markdown.nvim
+  -- config = function() require("nvim-markdown").setup {} end,
+}
