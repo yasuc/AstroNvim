0366ad7fa39372e4f2dc218813979a002b082f0f
IHA Yasushi <yasuc18@gmail.com>
2024-07-27 12:21:53 +0900

    feat: 🎸 add TypeScript support

--- init.lua
+++ init.lua
@@ -18,3 +18,17 @@ end
 require "lazy_setup"
 require "polish"
 require "config"
+
+vim.keymap.set(
+  { "n" },
+  "<C-k>",
+  function() require("lsp_signature").toggle_float_win() end,
+  { silent = true, noremap = true, desc = "toggle signature" }
+)
+
+vim.keymap.set(
+  { "n" },
+  "<Leader>k",
+  function() vim.lsp.buf.signature_help() end,
+  { silent = true, noremap = true, desc = "toggle signature" }
+)
