881a1a14c69916ad51e9f43732c18c9c92f910b2
IHA Yasushi <yasuc18@gmail.com>
2024-08-25 16:06:01 +0900

    fix: 🐛 modify for markdown plugin settings

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -8,14 +8,6 @@ return {
     local plugin = require("lazy.core.config").spec.plugins["markdown-preview.nvim"]
     vim.g.mkdp_filetypes = require("lazy.core.plugin").values(plugin, "ft", true)
   end,
-  keys = {
-    {
-      "<leader>,",
-      ft = "markdown",
-      "<cmd>MarkdownPreviewToggle<cr>",
-      desc = "Markdown Preview",
-    },
-  },
   build = function() vim.fn["mkdp#util#install"]() end,
   config = function() vim.cmd [[do FileType]] end,
 }
