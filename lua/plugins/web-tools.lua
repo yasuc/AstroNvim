2b2c98db4eebcc3c481c69f35e849af12a3b281e
IHA Yasushi <yasuc18@gmail.com>
2024-09-20 11:01:03 +0900

    feat: 🎸 add emmet mini-surround config

--- lua/plugins/web-tools.lua
+++ lua/plugins/web-tools.lua
@@ -1,4 +1,4 @@
-if false then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   {
     "ray-x/web-tools.nvim",
